3938ae42b884a33df16702083a5550b98b1fb448
akapikumin <akapikumin100piki@gmail.com>
2021-01-06 10:35:41 +0900

    --

--- js_folder/setting.js
+++ js_folder/setting.js
@@ -1,3 +1,10 @@
 'use strict'
 
 var config = JSON.parse(localStorage.getItem("config"));
+
+console.log(config)
+
+var is = document.getElementById("input_sleep_time");
+var iw = document.getElementById("input_wakeup_time");
+is.setAttribute("value", ('00' + config.sleep_hour).slice(-2) + ':' + ('00' + config.sleep_min).slice(-2));
+iw.setAttribute("value", ('00' + config.wakeup_hour).slice(-2) + ':' + ('00' + config.wakeup_min).slice(-2));
